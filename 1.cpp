3968e46d7b5145fced4b71d4776e8812a3709d8b
dtsipc <duteng504@gmail.com>
2014-07-13 16:32:22 +0800

    change

--- 1.cpp
+++ 1.cpp
@@ -12,3 +12,27 @@ public:
             s=out.substr(0,out.length()-1);
         }
 };
+
+/*
+#include <iostream>
+#include <sstream>
+#include <string>
+using namespace std;
+
+int main () {
+
+  int n,val;
+  string stringvalues;
+
+  stringvalues = "125 320 512 750 333";
+  istringstream iss(stringvalues);
+
+  for (n=0; n<5; n++)
+  {
+    iss >> val;
+    cout << val*2 << endl;
+  }
+
+  return 0;
+}
+*/
--- 2.cpp
+++ 2.cpp
@@ -37,3 +37,65 @@ public:
     return stack[0];
     }
 };
+
+
+/*
+#include<vector>
+#include<iostream>
+#include<stdlib.h>
+#include<sstream>
+
+using namespace std;
+
+void print_vector(vector<string> v) {
+    for (int index=0;index<v.size();++index)
+        cout<<v[index];
+    cout<<endl;
+}
+
+int evalRPN(vector<string> &token) {
+    vector<string>::iterator string_ite;
+    vector<int> stack;
+    int result;
+    for(string_ite=token.begin ();string_ite!=token.end ();string_ite++) {
+        string s = *string_ite;
+        if(s.compare("+") == 0 ||
+           s.compare("-") == 0 ||
+           s.compare("*") == 0 ||
+           s.compare("/") == 0) {
+            int a = stack[stack.size()-2];
+            int b = stack[stack.size()-1];
+
+            stack.pop_back();
+            stack.pop_back();
+            if(s.compare("+") == 0) {
+                result = a+b;
+            }
+            if(s.compare("-") == 0) {
+                result = a-b;
+            }
+            if(s.compare("*") == 0) {
+                result = a*b;
+            }
+            if(s.compare("/") == 0) {
+                result = a/b;
+            }
+            stack.push_back(result);
+        } else {
+            int number = atoi(s.c_str());
+            stack.push_back(number);
+        }
+
+    }
+    return stack[0];
+}
+
+int main() {
+    string str[] = {"8", "2", "/"};
+    vector<string> rpn(str,str+sizeof(str)/sizeof(str[0]));
+    int ret = evalRPN (rpn);
+    cout<<ret;
+    return 0;
+}
+
+*/
