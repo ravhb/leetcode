a71aa04c685d8b2065b3c5f187b2c5f396002710
Teng DU <duteng504@gmail.com>
2017-03-21 02:41:34 +0800

    add basics

---
+++ request-problem.js
@@ -0,0 +1,37 @@
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+
+var urls = [
+  'https://leetcode.com/problems/copy-list-with-random-pointer/#/description',
+];
+
+urls.forEach((url) => {
+  request(url, (err, response, body) => {
+    var $ = cheerio.load(body);
+    var textArr = $('.question-title h3').text().trim().split('. ');
+    var no = textArr[0];
+    var title = textArr[1];
+    var difficulty = $('.question-info li').eq(2).text().split(': ').pop();
+
+    var folderPath = `Algorithms/${title}`;
+    var filePath = `${folderPath}/README.md`;
+
+    fs.mkdir(folderPath, (err) => {
+      fs.open(filePath, 'wx', (err) => {
+        var content = [
+          `# [${title}](${url})`,
+          `###### No:\`${no}\``,
+          `###### Difficulty:\`${difficulty}\``,
+          '## JavaScript',
+          '',
+          '',
+          '```js',
+          '```',
+        ].join('\r\n') + '\r\n';
+
+        fs.writeFile(filePath, content);
+      });
+    });
+  });
+});
